fff5f7120e67856f4a267701f9f1f48e2d45c0cf
Yasmin <yasmin.amaral03@gmail.com>
2018-04-16 14:44:29 -0300

    E aí

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#git config --global user.name "Yasmin"
+#git config --global user.email "yasmin.amaral03@gmail.com"
+#mkdir <pasta>
+#cd <pasta>
+#touch arquivo.py 
+#git init
+#git status
+#git add arquivo.py
+#git commit -m "Mensagem"
+#git status
+#Alteração em alguma linda do arquivo.py
+
 a = 1
 b = 2
-print ("Resultado:")
+print ("1 + 2:") 
 print (a + b)
