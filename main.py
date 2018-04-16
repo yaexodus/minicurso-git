62a7291dbc947cae47ad9d5613f40ae82f374b9a
Yasmin <yasmin.amaral03@gmail.com>
2018-04-16 15:17:55 -0300

    main

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-#git config --global user.name "Yasmin"
-#git config --global user.email "yasmin.amaral03@gmail.com"
-#mkdir <pasta>
-#cd <pasta>
-#touch arquivo.py 
-#git init
-#git status
-#git add arquivo.py
-#git commit -m "Mensagem"
-#git status
-#Alteração em alguma linda do arquivo.py
-#git add arquivo.py para adicionar as alterações
-#git commit -m "Mensagem"
-#git checkout -b <nome-da-branch> já cria a branch e faz o checkout
-#cat arquivo.py
-#git checkout master
-#cat arquivo.py
-
 a = 1
 b = 2
 print ("1 + 2:") 
